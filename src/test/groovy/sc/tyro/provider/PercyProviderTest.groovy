d7bdd38beea35b967257a1e65e18cd5eec5357b0
altus34 <d.avenante@gmail.com>
2021-09-20 11:57:29 -0400

    WIP

--- src/test/groovy/sc/tyro/provider/PercyProviderTest.groovy
+++ src/test/groovy/sc/tyro/provider/PercyProviderTest.groovy
@@ -26,7 +26,7 @@ import static sc.tyro.core.Tyro.takeScreenshot
 
 @DisplayName("Screenshot Tests")
 class PercyProviderTest {
-    Percy percy = spy(new Percy(null))
+    private Percy percy = mock(Percy)
 
     @BeforeEach
     void before() {
@@ -35,7 +35,7 @@ class PercyProviderTest {
     }
 
     @Test
-    @DisplayName("Should take window screenshot with percy")
+    @DisplayName("Should take window screenshot with Percy")
     void percyScreenshot() {
         takeScreenshot('percy')
 
@@ -43,7 +43,7 @@ class PercyProviderTest {
     }
 
     @Test
-    @DisplayName("Should take window screenshot with percy specific config")
+    @DisplayName("Should take window screenshot with Percy specific config")
     void percyConfigScreenshot() {
         PercyProvider provider = screenshotProvider as PercyProvider
         provider.widths = List.of(1, 2, 3)
